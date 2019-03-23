89ad24647d0f73340658e8faa0231caf5fa3a772
SMARTBIGBOSS <425765498@qq.com>
2019-03-23 22:24:22 +0000

    Edit secertkey_config.js

--- configuration/secertkey_config.js
+++ configuration/secertkey_config.js
@@ -4,29 +4,10 @@ module.exports = {
     JWT_CUSTOMER_SECRET: 'customersecret',
     COOKIE_SECRET: 'cookiesecretkey',
     ACTIVE_CODE: 'acticecodekey',
-    AWS_Access_Key_ID: 'AKIAJCECAKT5WSPMFOCQ',//'AKIAIYTSZQPME6IKZRTA',
-    AWS_Secret_Access_Key: 'INmrMGNnfr9oi4UVH6RwVoF/Y796uo0mmvUD1+uE',//'jmC8zfePFqp2r15G5PIBhge5CBja+WwF3smbzpKf',
+    AWS_Access_Key_ID: 'YOURKEYID',
+    AWS_Secret_Access_Key: 'YOURACCESSKET',
     AwS_BUCKET: 'shopping.website'
-    // GOOGLE_CLOUD_PROJECT_ID: 'shoppingwebsite',
-    // GOOGLE_CLOUD_KEYFILE: ''
 };
 
-// {
-//     "Id": "Policy1552257704181",
-//     "Version": "2012-10-17",
-//     "Statement": [
-//     {
-//         "Sid": "Stmt1552257700183",
-//         "Action": "s3:*",
-//         "Effect": "Allow",
-//         "Resource": "arn:aws:s3:::shopping-website/*",
-//         "Principal": "*"
-//     }
-// ]
-// }
 
-// Access Key ID:
-//     AKIAIYTSZQPME6IKZRTA
-// Secret Access Key:
-//     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
 
