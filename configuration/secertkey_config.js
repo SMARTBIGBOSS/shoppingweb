6b28d86e304f1c0e44217f704d46ba990f435e4d
Anqi Li <425765498@qq.com>
2019-03-23 22:25:14 +0000

    Upload gitignore file

--- configuration/secertkey_config.js
+++ configuration/secertkey_config.js
@@ -7,26 +7,5 @@ module.exports = {
     AWS_Access_Key_ID: 'AKIAJCECAKT5WSPMFOCQ',//'AKIAIYTSZQPME6IKZRTA',
     AWS_Secret_Access_Key: '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',//'jmC8zfePFqp2r15G5PIBhge5CBja+WwF3smbzpKf',
     AwS_BUCKET: 'shopping.website'
-    // GOOGLE_CLOUD_PROJECT_ID: 'shoppingwebsite',
-    // GOOGLE_CLOUD_KEYFILE: ''
 };
-
-// {
-//     "Id": "Policy1552257704181",
-//     "Version": "2012-10-17",
-//     "Statement": [
-//     {
-//         "Sid": "Stmt1552257700183",
-//         "Action": "s3:*",
-//         "Effect": "Allow",
-//         "Resource": "arn:aws:s3:::shopping-website/*",
-//         "Principal": "*"
-//     }
-// ]
-// }
-
-// Access Key ID:
-//     AKIAIYTSZQPME6IKZRTA
-// Secret Access Key:
-//     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
 
